a4595a4a1a2c2cf4ab56fd5d672785cd38acacb2
Ismail <t.sukanmi@gmail.com>
2024-07-04 22:54:14 +0000

    assignment update

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,35 +1,42 @@
 import pandas as pd
-import datetime as dt
+from datetime import datetime 
+import batch
 
-
-categorical = ['PULocationID', 'DOLocationID']
-
-def prepare_data(filename, categorical):
-    df = pd.read_parquet(filename)
-    
-    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
-    df['duration'] = df.duration.dt.total_seconds() / 60
-
-    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
-
-    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
-    
-    return df
-
-
-from datetime import datetime
 
 def dt(hour, minute, second=0):
     return datetime(2023, 1, 1, hour, minute, second)
 
 
+    
+categorical = ['PULocationID', 'DOLocationID']
 
-data = [
-    (None, None, dt(1, 1), dt(1, 10)),
-    (1, 1, dt(1, 2), dt(1, 10)),
-    (1, None, dt(1, 2, 0), dt(1, 2, 59)),
-    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
-]
+def prepare_data():
+    
+    data = [
+                (None, None, dt(1, 1), dt(1, 10)),
+                (1, 1, dt(1, 2), dt(1, 10)),
+                (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+                (3, 4, dt(1, 2, 0), dt(2, 2, 1)),      
+            ]
 
-columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
-df = pd.DataFrame(data, columns=columns)
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    df = pd.DataFrame(data, columns=columns)
+    
+    actual_df = batch.read_data(df, categorical)
+
+
+    expected_data = [
+            ('-1', '-1', dt(1, 1), dt(1, 10), 9.0),
+            ('1', '1', dt(1, 2), dt(1, 10), 8.0),
+        ]
+
+    expected_columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'duration']
+    expected_df = pd.DataFrame(expected_data, columns=expected_columns)
+    
+    
+
+    # assert actual_df == expected_df
+    pd.testing.assert_frame_equal(actual_df.reset_index(drop=True), expected_df)
+if __name__ == "__main__":
+    prepare_data()
+    print("Tests passed successfully!")
